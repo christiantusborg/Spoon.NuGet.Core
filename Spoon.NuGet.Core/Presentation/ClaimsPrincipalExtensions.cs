5545037954412ed5ffff113a6a931aa4ce9b2596
Christian Tusborg <gitlab.com@terbium.dk>
2023-03-07 00:19:38 +0100

    Added ClaimsPrincipalExtensions->GetUserId

---
+++ Spoon.NuGet.Core/Presentation/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Spoon.NuGet.Core.Presentation;
+
+using System.Security.Claims;
+
+/// <summary>
+/// Extensions ClaimsPrincipal
+/// </summary>
+public static class ClaimsPrincipalExtensions
+{
+    /// <summary>
+    ///  Get UserID from ClaimsPrincipal->ClaimTypes.NameIdentifier
+    /// </summary>
+    /// <param name="principal"></param>
+    /// <returns></returns>
+    public static Guid GetUserId(this ClaimsPrincipal? principal)
+    {
+        var claim = principal?.FindFirst(ClaimTypes.NameIdentifier);
+        return !Guid.TryParse(claim?.Value, out var userId) ? Guid.Empty : userId;
+    }
+}
